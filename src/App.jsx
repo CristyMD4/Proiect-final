71db6b00998c3209bb5f498a73c2c98fcc462cb6
Miriam Hirbu <emailul_tau@example.com>
2026-03-12 15:50:45 +0200

    route

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import Book from './pages/Book'
 import { Routes } from 'react-router-dom'
 import Contact from './pages/Contact'
+import Gallery from './pages/Gallery'
 
 function App() {
   return (
    <Routes>
       <Route path="/book" element={<Book />}/>
       <Route path="/contact" elemenet={<Contact />}/>
-   </Routes>
+      <Route path="/gallery" elemenet={<Gallery />}/>
+   </Routes> 
   )
 }
 
