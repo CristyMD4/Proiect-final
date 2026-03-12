1faad22fd7228c00fefc5dbdae7b8617610a1fbd
Miriam Hirbu <emailul_tau@example.com>
2026-03-12 15:45:42 +0200

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Book from './pages/Book'
 import { Routes } from 'react-router-dom'
+import Contact from './pages/Contact'
 
 function App() {
   return (
    <Routes>
       <Route path="/book" element={<Book />}/>
+      <Route path="/contact" elemenet={<Contact />}/>
    </Routes>
   )
 }
